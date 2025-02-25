e9c52b59f6c1a1601f31a4d795406693ec229d04
melat234 <melatworktibebu@gmail.com>
2025-02-24 16:43:05 -0800

    Carosuel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,10 +1,8 @@
-
-
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import { img } from "./img/data"; 
+import { img } from "./img/data";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import styles from './Carousel.module.css'
+import styles from "./Carousel.module.css";
 
 function CarouselEffect() {
   return (
--- src/Components/Carousel/img/data.js
+++ src/Components/Carousel/img/data.js
@@ -1,8 +1,15 @@
+// import img1 from "./img1.jpeg";
+// import img2 from "./img2.jpeg";
+// import img3 from "./img3.jpeg";
+// import img4 from "./img4.jpeg";
+// import img5 from "./img5.jpeg";
+
+
+// export const img = [img1,img2,img3,img4,img5];
 import img1 from "./img1.jpeg";
 import img2 from "./img2.jpeg";
 import img3 from "./img3.jpeg";
 import img4 from "./img4.jpeg";
 import img5 from "./img5.jpeg";
 
-
-export const img = [img1,img2,img3,img4,img5];
+export const img = [img1, img2, img3, img4, img5];
